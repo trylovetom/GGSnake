98dba3c2c5f00fe3c0022e5651e3552087065253
張子晏 <try_love_tom@icloud.com>
2016-12-31 21:10:01 +0800

    first committee for ggsnake core

---
+++ GGSnake/GGSnake/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GGSnake
+//
+//  Created by 張子晏 on 2016/12/31.
+//  Copyright © 2016年 張子晏. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
